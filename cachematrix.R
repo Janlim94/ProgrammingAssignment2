1a1dd9fbf7f93b144024df05d4bb6a2d9b7bb372
Lim Soon Tat <black94@live.com.my>
2015-10-24 10:41:04 +0800

    Assignment 2 Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#makeCacheMatrix is function defined to creates a special "matrix" object that able to cache inverse matrix
+makeCacheMatrix<-function(x=matrix()){
+  #Defined variable matrix here 
+  matr <- NULL
+  #Defined function to get values x
+  set <- function(y) {
+    x <<- y
+    matr <<- NULL
+  }
+  #Defined function to search values of X
+  get <- function() x
+  #Defined function to set inverse matrix value
+  setinverse <- function(solve) matr <<- solve
+  #Defined function to get inverse matrix value
+  getinverse <- function() matr
+  #Defined function to listing down the sub-functions under the 'makeCacheMatrix' function
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+#cacheSolve is function defined to computes the inverse of the special "matrix" obtain from makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  #obtain inverse matrix calculated previously 
+  matr <- x$getinverse()
+  #Checking for null value, if value returned is not null value,computational steps below will skip and return value of matr
+  if(!is.null(matr)) {
+    message("obtaining cached data")
+    return(matr)
+  }
+  #if condition is false, proceed to below steps
+  data <- x$get()
+  matr <- solve(data, ...)
+  x$setinverse(matr)
+  matr
 }
